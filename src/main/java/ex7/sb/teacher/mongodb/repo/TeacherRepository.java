666dbc20d11fa97c25e36356ac9d4dfccd8fd5eb
ilker <ilkerpublic@gmail.com>
2018-04-19 20:18:45 -0400

    Added example queries

--- src/main/java/ex7/sb/teacher/mongodb/repo/TeacherRepository.java
+++ src/main/java/ex7/sb/teacher/mongodb/repo/TeacherRepository.java
@@ -3,16 +3,29 @@
  */
 package ex7.sb.teacher.mongodb.repo;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import ex7.sb.teacher.mongodb.model.Teacher;
 
 /**
+ * Some doc and examples of queries
+ * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/
+ * https://lishman.io/spring-data-jpa-repository-queries
+ * 
  * @author ilker
  *
  */
 @Repository
 public interface TeacherRepository extends MongoRepository<Teacher, String> {
-
+	public Optional<List<Teacher>> findByTeacherId(String teacherId);
+	public Optional<List<Teacher>> findByAgeLessThan(int age);
+	public Optional<List<Teacher>> findByNameOrLastnameOrderByAgeDesc(String name, String lastname);
+	public Optional<List<Teacher>> findFirst3ByTitle(String title);
+	public Optional<List<Teacher>> findByNameStartingWith(String name);
+	public void deleteByTeacherId(String teacherId);
+	
 }
