9a37f26d23fa4656ff7e19d71c42974aa29c2291
ilker <ilkerpublic@gmail.com>
2018-04-23 17:09:41 -0400

    added couple of example native queries

--- src/main/java/ex7/sb/student/jpa/repo/StudentRepository.java
+++ src/main/java/ex7/sb/student/jpa/repo/StudentRepository.java
@@ -7,10 +7,19 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import ex7.sb.student.jpa.model.Student;
 
 /**
+ * Couple of example native queries
+ *   @Query("select s from Student s where lower(s.name) = lower(:studentName)")
+ *   public Country findByNameIgnoreCaseQuery(@Param("studentName") String studentName);
+ * 
+ *   @Query("select s from Student s where c.name = ?1")
+ *   public Country findByNameQueryPositionalParam(String studentName);
+ * 
  * @author ilker
  *
  */
